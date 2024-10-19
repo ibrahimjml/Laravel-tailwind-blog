e2ccaf60563da4b31798b0ff1e10f085c7607f60
Ibrahim jamal <anony.jjml@gmail.com>
2024-10-19 22:39:07 +0200

    replace Http

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\http\Middleware\CheckIfBlocked::class,
+            \App\Http\Middleware\CheckIfBlocked::class,
         ],
 
         'api' => [
