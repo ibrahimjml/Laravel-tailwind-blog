74a279b46eac63ed6e4883f4159025585bf9d841
ibrahimjml <anony.jjml@gmail.com>
2025-09-19 14:40:26 +0300

    fix: removed url() from og_image in function generateDefault()

--- resources/views/hashtags/show.blade.php
+++ resources/views/hashtags/show.blade.php
@@ -11,7 +11,9 @@
   <h1 class=" text-4xl p-36 font-semibold text-center w-54">No Posts Yet</h1>
 
 @else
-<h1 class=" text-4xl  p-5 font-semibold text-center text-gray-500 w-54">Showing recent posts with # <span class="text-black">{{$hashtag->name}}</span></h1>
+<div class="w-full p-5 mt-1 bg-black">
+  <h1 class=" text-4xl  p-5 font-semibold text-center text-white ">Showing recent posts with <span class="text-white bg-yellow-500 p-3 rounded-lg">#{{$hashtag->name}}</span></h1>
+</div>
 @foreach ($posts as $post)
     
 <x-postcard :post="$post" :authFollowings="$authFollowings"/>
